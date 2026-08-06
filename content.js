599af9bb67cecdf2782b459ba7ba4ceedff3a79a
美川 颯良 <135324944+sora-mikawa@users.noreply.github.com>
2026-08-06 16:44:35 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,2 +1,28 @@
-﻿const loginLink = document.querySelector('a[href="https://kadai-moodle.kagawa-u.ac.jp/login/index.php"]');
-if (loginLink) loginLink.click();
+﻿// トップページなど、ログインページ以外にいる場合のみリンクをクリックする
+if (!window.location.href.includes("/login/index.php")) {
+    const loginLink = document.querySelector(
+        'a[href="https://kadai-moodle.kagawa-u.ac.jp/login/index.php"]'
+    );
+    if (loginLink) loginLink.click();
+}
+
+// ログインページ用の処理
+const waitForLoginButton = () => {
+    const loginButton = document.querySelector("#loginbtn");
+
+    if (loginButton) {
+        loginButton.focus();
+        console.log("ログインボタンにフォーカスしました");
+    } else {
+        setTimeout(waitForLoginButton, 100);
+    }
+};
+
+if (window.location.href.includes("/login/index.php")) {
+    if (document.readyState === "complete") {
+        waitForLoginButton();
+    } else {
+        window.addEventListener("load", waitForLoginButton);
+    }
+}
+
